11dbee82d0ece4e01de27929d99edb9bddec61b6
Camilo Santander <camilosantander@usantotomas.edu.co>
2023-02-09 21:15:53 -0500

    Basic creation of the user

--- main.py
+++ main.py
@@ -1,2 +1,13 @@
 import requests
 
+pixela_endpoint = "https://pixe.la/v1/users"
+user_parameters = {
+    "token": "tokenization",
+    "username": "cams",
+    "notMinor": "yes",
+    "agreeTermsOfService": "yes"
+}
+# response = requests.post(url=pixela_endpoint, json=user_parameters)
+# print(response.text)
+
+
